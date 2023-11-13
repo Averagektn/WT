137fc82fad6d7953a464b1a9b640b3e3396c76fd
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-11-13 20:21:21 +0300

    Moved jsps to safer place

--- lab2/myCoolStore/src/by/bsuir/mycoolsite/controller/JSPPageName.java
+++ lab2/myCoolStore/src/by/bsuir/mycoolsite/controller/JSPPageName.java
@@ -4,12 +4,12 @@ public class JSPPageName {
     private JSPPageName() {
     }
 
-    public static final String PAGE_BAN = "/jsp/ban.jsp";
-    public static final String PAGE_LIBRARY = "/jsp/library.jsp";
-    public static final String PAGE_CART = "/jsp/cart.jsp";
+    public static final String PAGE_BAN = "/WEB-INF/jsp/ban.jsp";
+    public static final String PAGE_LIBRARY = "/WEB-INF/jsp/library.jsp";
+    public static final String PAGE_CART = "/WEB-INF/jsp/cart.jsp";
     public static final String PAGE_MAIN = "/index.jsp";
-    public static final String PAGE_FILM = "/jsp/customerFilm.jsp";
-    public static final String PAGE_ADMIN_FILM = "/jsp/adminFilm.jsp";
-    public static final String PAGE_BAN_LIST = "/jsp/banList.jsp";
-    public static final String PAGE_ERROR = "/myCoolStore/error.jsp";
+    public static final String PAGE_FILM = "/WEB-INF/jsp/customerFilm.jsp";
+    public static final String PAGE_ADMIN_FILM = "/WEB-INF/jsp/adminFilm.jsp";
+    public static final String PAGE_BAN_LIST = "/WEB-INF/jsp/banList.jsp";
+    public static final String PAGE_ERROR = "/WEB-INF/jsp/error.jsp";
 }
